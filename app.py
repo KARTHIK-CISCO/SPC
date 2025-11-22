f25bca76d822d9bbf0b1b0613b36ab4f9b514c45
KARTHIK-CISCO <85832969+KARTHIK-CISCO@users.noreply.github.com>
2025-11-22 10:43:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ st.write("Upload your stock dataset (Date, Open, High, Low, Close, Volume).")
 uploaded_file = st.file_uploader("Upload CSV File", type=['csv'])
 
 if uploaded_file:
-    df = pd.read_csv("/content/AAPL.csv")
+    df = pd.read_csv(uploaded_file)
+
 
     st.subheader("📄 Dataset Preview")
     st.dataframe(df.head())
