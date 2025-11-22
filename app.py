8b890521462b268ff0ef70ad4b7ce7905af461c3
KARTHIK-CISCO <85832969+KARTHIK-CISCO@users.noreply.github.com>
2025-11-22 13:04:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ uploaded_file = st.file_uploader("Upload CSV File", type=['csv'])
 
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
+    df = df.sort_values('Date')
     st.subheader("📄 Dataset Preview")
     st.dataframe(df.head())
 
